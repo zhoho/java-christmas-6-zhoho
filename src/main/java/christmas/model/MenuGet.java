8eaee4322afc22a297430292eb88dfa511ce00e8
Zho <chlwlgh1011@naver.com>
2023-11-13 10:53:05 +0900

    feat: 유효하지 않는 메뉴 입력받으면 예외처리

--- src/main/java/christmas/model/MenuGet.java
+++ src/main/java/christmas/model/MenuGet.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import christmas.constant.Constants;
 import christmas.view.OutputView;
 
 public class MenuGet {
@@ -10,7 +11,16 @@ public class MenuGet {
             parts = order.split("-");
             String itemName = parts[0];
             int quantity = Integer.parseInt(parts[1]);
+            menuValidate(itemName);
             OutputView.printMenu(itemName, quantity);
+        }
+    }
+
+    public static void menuValidate(String itemName) {
+        try {
+            Constants.Menu menu = Constants.Menu.valueOf(itemName);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 }
